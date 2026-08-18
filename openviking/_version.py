dfd2e954240204b465933007ba99476e6325023c
skloxo <skloxo@users.noreply.github.com>
2026-08-18 17:38:35 +0800

    chore: sync generated _version.py to 1.3.8.dev0

--- openviking/_version.py
+++ openviking/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev189'
-__version_tuple__ = version_tuple = (0, 1, 'dev189')
+__version__ = version = '1.3.8.dev0'
+__version_tuple__ = version_tuple = (1, 3, 8, 'dev0')
 
-__commit_id__ = commit_id = 'gbcd358294'
+__commit_id__ = commit_id = 'g5185c509'
