2b7f53006910e0e08bfefe759f0ccad573d1da96
skloxo <skloxo@users.noreply.github.com>
2026-09-11 18:27:43 +0800

    fix(mimo): sync messages.transform plugin to 2080Ti Windows, isolate ELECTRON_RUN_AS_NODE and restart 4096 engine (v1.4.110)

--- openviking/_version.py
+++ openviking/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '1.4.107'
-__version_tuple__ = version_tuple = (1, 4, 107)
+__version__ = version = '1.4.110'
+__version_tuple__ = version_tuple = (1, 4, 110)
 
 __commit_id__ = commit_id = 'g5d48b987e'
