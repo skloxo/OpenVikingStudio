06ee466f70b99f9589edd3cdf9c53b6bd78aa3eb
skloxo <skloxo@users.noreply.github.com>
2026-09-11 13:55:53 +0800

    chore: update backend _version.py to 1.4.103

--- openviking/_version.py
+++ openviking/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '1.4.91'
-__version_tuple__ = version_tuple = (1, 4, 87)
+__version__ = version = '1.4.103'
+__version_tuple__ = version_tuple = (1, 4, 103)
 
 __commit_id__ = commit_id = 'g5d48b987e'
